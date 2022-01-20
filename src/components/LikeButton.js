2ad560d3856760dbd9e6ea66b80bc067ede4940d
Joshua Sells <josh@Joshuas-MacBook-Pro.local>
2022-01-19 20:02:46 -0800

    like import fix

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ThumbUpIcon } from "@heroicons/react/solid";
 
 // TODO record likes to local storage by passing image id
