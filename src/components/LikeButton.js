06c66769ae7e155d25591c7548c5b4381b832c3e
Joshua Sells <josh@Joshuas-MacBook-Pro.local>
2022-01-19 20:01:07 -0800

    like button

---
+++ src/components/LikeButton.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { ThumbUpIcon } from "@heroicons/react/solid";
+
+// TODO record likes to local storage by passing image id
+const LikeButton = () => {
+  const [isLiked, setLike] = useState(false);
+  const handleLike = () => {
+    if (!isLiked) {
+      setLike(true);
+    } else {
+      setLike(false);
+    }
+  };
+  return (
+    <button>
+      <ThumbUpIcon
+        onClick={handleLike}
+        className={`h-8 w-8 ${isLiked ? "text-red-500" : ""}`}
+      />
+    </button>
+  );
+};
+
+export default LikeButton;
