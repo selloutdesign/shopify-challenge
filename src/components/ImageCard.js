0a6704249d07a75a4e38682d547ff5bcef73ad3c
Joshua Sells <josh@Joshuas-MacBook-Pro.local>
2022-01-19 18:38:15 -0800

    This is a new commit for what I originally planned to be amended

---
+++ src/components/ImageCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import moment from "moment";
+
+const ImageCard = ({ image }) => {
+  const truncate = (input) =>
+    input?.length > 300 ? `${input.substring(0, 254)}...` : input;
+
+  if (image.hasOwnProperty("links")) {
+    return (
+      <div className="rounded overflow-hidden shadow-lg">
+        <img src={image.links[0].href} alt="" className="w-full" />
+        <p className="text-gray-900 mt-2 text-lg">{image.data[0].title}</p>
+        <p className="text-sm">
+          {moment(image.data[0].date_created).format("MMMM Do YYYY")}
+        </p>
+        <p className="text-sm">{truncate(image.data[0].description)}</p>
+      </div>
+    );
+  }
+  return <div></div>;
+};
+
+export default ImageCard;
